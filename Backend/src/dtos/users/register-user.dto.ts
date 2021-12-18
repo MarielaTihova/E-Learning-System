42eafc021bc1513d2aa88de24083409b60418975
m.tihova <mariela.tihova@gmail.com>
2021-12-18 14:30:38 +0200

    Project folders

---
+++ Backend/src/dtos/users/register-user.dto.ts
@@ -0,0 +1,19 @@
+
+import { IsString, Length, Matches } from "class-validator"
+export class RegisterUserDTO {
+    @IsString()
+    @Length(5, 30)
+    username: string;
+    @IsString()
+    @Length(5, 30)
+    personalName: string;
+    @IsString()
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/, {
+        message:
+            'The password must be minimum five characters, at least one letter and one number',
+    })
+
+    password: string;
+    @IsString()
+    avatar: string;
+}
---
+++ Backend/src/models/enums/user-role.ts
@@ -0,0 +1,5 @@
+export enum UserRole {
+  Student = 1,
+  Admin = 2,
+  Teacher = 3
+}
